97b65a1786b6d6990a35f4bc47f7e2bd900ff3bc
tk <tekeste.kidanu@gmail.com>
2017-07-07 19:07:15 -0400

    finish the button :scream_cat:

--- extension/content.js
+++ extension/content.js
@@ -2,12 +2,25 @@
 * Product Hunt Back To Top Button (PHBTTB)
 */
 
-const PHBTTB = {
-  run: () => {
-
-  },
+const run = () => {
+  const bodyEl = document.body;
+  const buttonEl = createButton();
+  bodyEl.appendChild(buttonEl);
 };
 
-window.onload  = () => {
-  PHBTTB.run();
+const createButton = () => {
+  const buttonTemplate  = `<div class="tk-back-to-top">
+                            <a href="#" title="To the Top" >
+                             <img src="http://i.imgur.com/4VFasrG.png"/>
+                            </a>
+                          </div>`;
+
+  let buttonEl = document.createElement('div');
+      buttonEl.innerHTML = buttonTemplate;
+
+  return buttonEl;
+};
+
+window.onload = () => {
+  run();
 };
